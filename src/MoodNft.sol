683a273e0a5dc38776a5c9d58b6d6d1930c06b44
Fabian Diaz <fabiandiaz@Fabians-MacBook-Pro.local>
2024-09-11 13:16:12 -0300

    last changes

---
+++ src/MoodNft.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.27;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft {
+    uint256 private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
+    constructor(
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("MoodNft", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    function mintNft() public {
+        _safeNft(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+}
